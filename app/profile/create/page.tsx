a5ee1d826c84749306c746320fcc8ff84e81cc24
dechawat <dechawat.kl@hotmail.com>
2024-11-24 10:31:42 +0700

    ทำการแก้ไขตรง Form ไปใช้ component แทน

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,12 +1,12 @@
-
-import { Button } from "@/components/ui/button"
 import { SubmitButton } from "@/components/ui/form/Buttons"
+import FormContainer from "@/components/ui/form/FormContainer"
 import FormInput from "@/components/ui/form/FormInput"
 
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server'
   const firstName = formData.get('firstName') as string
+  return { message: 'Create Profile Success!!!' }
 }
 
 
@@ -16,17 +16,17 @@ const CreateProfile = () => {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new User</h1>
       <div className="border p-8 rounded">
-        <form action={createProfileAction}>
+        <FormContainer action={createProfileAction}>
           <div className="grid md:grid-cols-2 gap-4 mt-4">
-            <FormInput name="firstname" label="First Name" type="text" placeholder="Enter you First Name" required/>
-            <FormInput name="lastname" label="Last Name" type="text" placeholder="Enter you Last Name" required/>
-            <FormInput name="username" label="Username" type="text" placeholder="Enter you Username" required/>
-            <FormInput name="password" label="Password" type="password" placeholder="Enter you Password" required/>
-            <FormInput name="confirmpassword" label="Confirm Password" type="password" placeholder="Enter you Confirm Password" required/>
-            <FormInput name="email" label="Email" type="email" placeholder="Enter you Email" required/>
+            <FormInput name="firstname" label="First Name" type="text" placeholder="Enter you First Name" required />
+            <FormInput name="lastname" label="Last Name" type="text" placeholder="Enter you Last Name" required />
+            <FormInput name="username" label="Username" type="text" placeholder="Enter you Username" required />
+            <FormInput name="password" label="Password" type="password" placeholder="Enter you Password" required />
+            <FormInput name="confirmpassword" label="Confirm Password" type="password" placeholder="Enter you Confirm Password" required />
+            <FormInput name="email" label="Email" type="email" placeholder="Enter you Email" required />
           </div>
-          <SubmitButton text="Create Profile" size="lg"/>
-        </form>
+          <SubmitButton text="Create Profile" size="lg" />
+        </FormContainer>
       </div>
 
 

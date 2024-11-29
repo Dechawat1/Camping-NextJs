f9472287e69943d1eb6ef25d83c4aeecb936f6f7
dechawat <dechawat.kl@hotmail.com>
2024-11-29 13:24:02 +0700

    ทำการเพิ่ม page create Landmark

---
+++ app/camp/create/page.tsx
@@ -0,0 +1,28 @@
+import { createLankmarkAction} from "@/actions/actions"
+import { SubmitButton } from "@/components/ui/form/Buttons"
+import FormContainer from "@/components/ui/form/FormContainer"
+import FormInput from "@/components/ui/form/FormInput"
+
+
+const CreateLankmark = () => {
+
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">Create Landmark</h1>
+      <div className="border p-8 rounded">
+        <FormContainer action={createLankmarkAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            <FormInput name="name" 
+            label="Landmark Name" type="text" 
+            placeholder="Enter you Lankmark Name" 
+            required />
+          </div>
+          <SubmitButton text="create Landmark" size="lg" />
+        </FormContainer>
+      </div>
+
+
+    </section>
+  )
+}
+export default CreateLankmark

5f09d77d24c2e969794dacf39565cb27019d3c49
dechawat <dechawat.kl@hotmail.com>
2024-12-03 19:31:27 +0700

    ทำการเช็คว่ามีข้อมูลไหม ถ้าไม่มีให้แสดงหน้าโหลดดิ่งไม่มีข้อมูล

--- components/home/LandmarkContainer.tsx
+++ components/home/LandmarkContainer.tsx
@@ -3,18 +3,26 @@ import LandmarkList from "./LandmarkList"
 import { LandmarkCardProps } from "@/utils/types"
 import Hero from "../hero/Hero"
 import CategoriesList from "./CategoriesList"
+import NoDataPage from "../ืnodatapage/NoDataPage"
+
 
 
 const LandmarkContainer = async ({ search, category }: { search?: string, category?: string }) => {
-  const landmarks: LandmarkCardProps[] = await fetchLandmarks({ search,category })
+  const landmarks: LandmarkCardProps[] = await fetchLandmarks({ search, category })
   // console.log(landmarks)
 
-  return (
-    <div>
-      <Hero landmarks={landmarks} />
-      <CategoriesList search={search} category={category}/>
-      <LandmarkList landmarks={landmarks} />
-    </div>
-  )
+  if (!landmarks || landmarks.length === 0) {
+    return <NoDataPage/>
+  } else {
+    return (
+      <div>
+        <Hero landmarks={landmarks} />
+        <CategoriesList search={search} category={category} />
+        <LandmarkList landmarks={landmarks} />
+      </div>
+    )
+  }
+
+
 }
 export default LandmarkContainer

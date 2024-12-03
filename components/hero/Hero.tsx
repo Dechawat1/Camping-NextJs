65cfaa92c4dbc8f43465fca153918ab130a5ca40
dechawat <dechawat.kl@hotmail.com>
2024-12-03 17:53:33 +0700

    ทำการเพิ่มข้อความในรูปภาพที่แสดงแนะนำ

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -7,6 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Otherinfo from "./Otherinfo";
 
 const Hero = ({ landmarks }: { landmarks: LandmarkCardProps[] }) => {
     return (
@@ -34,6 +35,14 @@ const Hero = ({ landmarks }: { landmarks: LandmarkCardProps[] }) => {
                                     <img
                                         className="w-full h-[600px] object-cover brightness-75 group-hover:brightness-50 transition-all duration-100"
                                         src={item.image} />
+
+                                </div>
+                                <div className="absolute bottom-0 left-0 z-50">
+                                    <div
+                                        className="col-span-4 mb-4 flex h-full flex-1 justify-end px-5 md:mb-4 md:justify-end md:px-10"
+                                    >
+                                        <Otherinfo landmark={item} />
+                                    </div>
 
                                 </div>
                             </SwiperSlide>
